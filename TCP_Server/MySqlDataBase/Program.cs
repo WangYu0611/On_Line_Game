f447ac1e3f2882858c91496b11a01d4b172d8b20
汪煜 <331508364@qq.com>
2018-12-11 13:02:08 +0800

    增加数据库防注入功能 增肌数据库删除和更新功能

--- TCP_Server/MySqlDataBase/Program.cs
+++ TCP_Server/MySqlDataBase/Program.cs
@@ -52,10 +52,28 @@ namespace MySqlDataBase
 
             //插入
             string useName2 = "caiyinhua";
-            string passWord2 = "123456";
-            MySqlCommand cmd2 = new MySqlCommand("insert into user set UseName='" + useName2 + "'" + ",Password='" + passWord2 + "' ", SQL);
+            string passWord2 = "12345678";
 
+            //容易被注入数据库
+            //MySqlCommand cmd2 = new MySqlCommand("insert into user set UseName='" + useName2 + "'" + ",Password='" + passWord2 + "' ", SQL);
+
+            //防止注入
+            MySqlCommand cmd2 = new MySqlCommand("insert into user set UseName=@un,Password=@pwd", SQL);
+            cmd2.Parameters.AddWithValue("un", useName2);
+            cmd2.Parameters.AddWithValue("pwd", passWord2);
             cmd2.ExecuteNonQuery();
+
+
+            //删除
+            MySqlCommand cmd3 = new MySqlCommand("delete from user where id =@id", SQL);
+            cmd3.Parameters.AddWithValue("id", 4);
+            cmd3.ExecuteNonQuery();
+
+
+            //更新
+            MySqlCommand cmd4 = new MySqlCommand("update user set password = @pwd where id =1", SQL);
+            cmd4.Parameters.AddWithValue("pwd", "cyh520");
+            cmd4.ExecuteNonQuery();
 
 
             SQL.Close();
